cf23dc1aa88140a5a987e57e122df9c616d5c00d
Jhonatan Tipan <tuemail@github.com>
2026-04-19 22:00:47 -0500

    cambio en index

--- server/index.js
+++ server/index.js
@@ -19,13 +19,13 @@ app.use('/api/tasks', require('./routes/tasks'));
 
 // Servir React en producción
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
   });
 } else {
   app.get('/', (req, res) => res.send('API corriendo en modo desarrollo'));
 }
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`))
